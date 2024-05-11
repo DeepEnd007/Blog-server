8622e0436b898e8fac63dd3a7783043645c8da77
DeepEnd007 <yihuihu56@gmail.com>
2024-05-11 23:49:40 +1200

    still working on auth.js

--- controllers/auth.js
+++ controllers/auth.js
@@ -3,7 +3,7 @@ import jwt from "jsonwebtoken";
 import User from "../models/User.js";
 
 /* Register User*/
-export const register = async (res, res) => {
+export const register = async (req, res) => {
   try {
     const {
       firstName,
@@ -18,5 +18,20 @@ export const register = async (res, res) => {
 
     const salt = await bcrypt.genSalt();
     const passwordHash = await bcrypt.hash(password, salt);
+
+    const newUser = new User({
+      firstName,
+      lastName,
+      email,
+      password: passwordHash,
+      picturePath,
+      friends,
+      location,
+      occupation,
+      viewedProfile: Math.floor(Math.random() * 10000),
+      impression: Math.floor(Math.random() * 10000),
+    });
+    const savedUser = await newUser.save();
+    res.status(201).json(savedUser);
   } catch (err) {}
 };
