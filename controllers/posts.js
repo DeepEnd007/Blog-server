4cb077fb6ef50e5047de0c335b350e7e2da1e5dc
DeepEnd007 <yihuihu56@gmail.com>
2024-05-13 23:01:48 +1200

    working on Posts

---
+++ controllers/posts.js
@@ -0,0 +1,13 @@
+import Post from "../models/Post.js";
+import User from "../models/User.js";
+
+/*Create*/
+
+export const createPost = async (req, res) => {
+  try {
+    const { userId, description, picturePath } = req.body;
+    const user = await User.findById(userId);
+  } catch (err) {
+    res.status(409).json({ message: err.message });
+  }
+};
--- index.js
+++ index.js
@@ -10,7 +10,9 @@ import path from "path";
 import { fileURLToPath } from "url";
 import authRoutes from "./routes/auth.js";
 import userRoutes from "./routes/users.js";
+import postRoutes from "./routes/posts.js";
 import { register } from "./controllers/auth.js";
+import { createPost } from "./controllers/posts.js";
 import { verifyToken } from "./middleware/auth.js";
 
 // CONFIGURATION
@@ -41,6 +43,7 @@ const upload = multer({ storage });
 
 /* Route with Files */
 app.post("/auth/register", upload.single("picture"), register);
+app.post("/posts", verifyToken, upload.single("picture"), createPost);
 
 /* Routes */
 app.use("/auth", authRoutes);
